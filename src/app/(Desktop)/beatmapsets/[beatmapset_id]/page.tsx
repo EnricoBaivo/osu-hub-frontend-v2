a67deead1ccef281ec3d6f75774265250a49264c
Enrico Bono <enrico.bono.home@gmail.com>
2024-07-28 18:17:38 +0200

    fix: auth check on beatmapsetspage

--- src/app/(Desktop)/beatmapsets/[beatmapset_id]/page.tsx
+++ src/app/(Desktop)/beatmapsets/[beatmapset_id]/page.tsx
@@ -1,6 +1,9 @@
 import { api } from '@/trpc/server'
 import React from 'react'
 import AnimeDetail from './AnimieDetailComponent';
+import { auth } from '@/server/auth';
+import { redirect } from 'next/navigation';
+import { env } from '@/env.mjs';
 
 export default async function page({
     params,
@@ -9,6 +12,10 @@ export default async function page({
     params?: { beatmapset_id: string };
     searchParams?: Record<string, string>;
 }) {
+    const session = await auth()
+    if (!session) {
+        return  redirect(env.TO_LOGIN_PAGE);
+    }
     const aniListMedia = await api.beatmapset.getAniListMedia.query({
         beatmapset_id: Number(params?.beatmapset_id)
     })
